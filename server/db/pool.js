de8ce861d7cc635f0e02905c934ffbf9a2428594
blueskinlizard <blueskinlizard@gmail.com>
2025-04-09 17:38:05 -0500

    Stopped tracking pool.js local variables

--- server/db/pool.js
+++
@@ -1,9 +0,0 @@
-const Pool = require("pg");
-
-module.exports = new Pool({
-    host: "localhost", 
-    user: "postgres",
-    database: "pokemon_tracker_database",
-    password: "",
-    port: 5432 
-  });
