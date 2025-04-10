4478dc8037c501c932e663fc7b147781fc88ffcb
blueskinlizard <blueskinlizard@gmail.com>
2025-04-10 05:27:33 -0500

    Added pokemon table initialization script

---
+++ server/db/createPokemonAttributes.js
@@ -0,0 +1,20 @@
+const { db } = require(`./queries`)
+
+async function createPokemonAttributes(){
+    //Yes this is ripped off my React Memory Card code on Github, check it out!
+    try{
+        const promises = Array.from({ length: 50 }, async (_, i) => {
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`);
+            return await res.json();
+        })
+        const results = await Promise.all(promises);
+        for (const result of results) {
+            await db.insertPokemonAttribute(result.name, result.id);
+        }
+    }
+    catch{
+        console.log("Error fetching Pokemon data");
+    }
+}
+
+createPokemonAttributes();
