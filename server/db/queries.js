8a30b89a078c9709c007e7906b82e76dd4a2f008
blueskinlizard <blueskinlizard@gmail.com>
2025-04-09 19:02:19 -0500

    Basic db query functions set up

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,10 +1,26 @@
 const pool = require("./pool");
 
-async function getAllPokemon(username){
-    const { ownedPokemon } = await pool.query("SELECT * FROM (username)", [username]);
+async function getAllPokemon(userID){
+    const { ownedPokemon } = await pool.query("SELECT pokemonOwned FROM uservalues WHERE userID = $1", [userID]);
     return ownedPokemon;
 }
 
-async function addPokemon(username, pokemonIDAdded){
-    await pool.query("INSERT (pokemonIDAddded) INTO (username) VALUES $1", [username, pokemonIDAdded]);
+async function addPokemonUser(pokemonToAdd, userID) {
+    await pool.query("UPDATE uservalues SET pokemonOwned = array_append(pokemonOwned, $1) WHERE userID = $2", [pokemonToAdd, userID]);
 }
+
+async function setPokemonDescription(pokemonToChange, description, userID) {
+    await pool.query("INSERT INTO pokemon_descriptions (userID, pokemonID, pokemonDescription) VALUES ($1, $2, $3)", [userID, pokemonToChange, description]);
+}
+
+async function getPokemonDescription(pokemonToRetrieve, userID){
+    const { pokemonDescription } = await pool.query("SELECT pokemonDescription FROM pokemon_descriptions WHERE userID = $1 AND pokemonID = $2", [userID, pokemonToRetrieve]);
+    return pokemonDescription;
+}
+
+async function getUserPassword(userName){
+    const { password } = await pool.query("SELECT userPassword from userValues WHERE userName = $1", [userName]);
+    return password;
+}
+//idea is that we enter the pokemon name on the fontend, which react will try to fetch from pokemon API.
+// When fetched, we will set an image component along with the user inputted description
